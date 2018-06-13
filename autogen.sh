cae84f8219caf513162e67e566045710106e49b6
Jonathan Davies <jonathan.davies@nutanix.com>
2018-06-13 10:17:51 +0000

    Add configure flags

--- autogen.sh
+++ autogen.sh
@@ -24,5 +24,5 @@ echo "----------------------------------------------------------------"
 echo "Initialized build system. For a common configuration please run:"
 echo "----------------------------------------------------------------"
 echo
-echo "./configure CFLAGS='-g -O2' $args"
+echo "./configure CFLAGS='-g -O2 -lrt' $args --enable-logging --enable-debug"
 echo
